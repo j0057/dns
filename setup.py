13ae553c6a3da9e066e5013b6c36f531bd9db65a
Joost Molenaar <j.j.molenaar@gmail.com>
2015-01-22 22:22:24 +0100

    Update to new setuptools-version-command

--- setup.py
+++ setup.py
@@ -12,8 +12,7 @@ setup(
     author_email='j.j.molenaar@gmail.com',
     url='https://github.com/j0057/dns',
     name='dns',
-    version_command='git describe',
-    version_command_pep440='git-dev',
+    version_command=('git describe', 'pep440-git-dev'),
     packages=['dns'],
     data_files=[ (root, map(lambda f: root + '/' + f, files))
                  for src_dir in static_dirs
